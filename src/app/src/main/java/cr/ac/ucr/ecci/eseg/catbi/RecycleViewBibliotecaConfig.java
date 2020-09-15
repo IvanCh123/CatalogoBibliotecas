83254c2992e210640d15471ac6704f22484f4eb9
jvs1991 <josue.valverde1@gmail.com>
2020-09-15 15:45:39 -0600

    CQN-18
    CQN-18 Lista de bibliotecas
    
    Conexion exitosa con la base en fireBase

---
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/RecycleViewBibliotecaConfig.java
@@ -0,0 +1,17 @@
+package cr.ac.ucr.ecci.eseg.catbi;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecycleViewBibliotecaConfig {
+    private Context contexto;
+
+
+    /*class BibliotecaItem extends RecyclerView.ViewHolder {
+        private TextView biblioteca_horario;
+        private TextView biblioteca_nombre;
+        private TextView biblioteca_telefono;
+    }*/
+}
