2d05edf0d8a966825755822413c1b1bcbdbecac9
Sebastian Otarola <otarolasebas@hotmail.com>
2020-09-19 22:05:18 -0600

    CNQ - 17 Buscador básico Primer intento para redirección a un fragmento específico de una actividad.

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ResultadosBusqueda.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ResultadosBusqueda.java
@@ -2,6 +2,9 @@ package cr.ac.ucr.ecci.eseg.catbi;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -27,6 +30,26 @@ public class ResultadosBusqueda extends AppCompatActivity {
         String message = intent.getStringExtra(MESSAGE_KEY);
         TextView textView = (TextView) findViewById(R.id.txtPalabra);
         textView.setText(message);
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemMenu = item.getItemId();
+                Intent intentActPrincipal = new Intent(getBaseContext(), MainActivity.class);
+
+                if(itemMenu == R.id.nav_busqueda){
+                    Fragment mFragment = null;
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    //fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
+
+                }else if (itemMenu == R.id.nav_ubicacion){
+
+                }else if (itemMenu == R.id.nav_preguntas){
+
+                }
+                return false;
+            }
+        });
+
 
 
 
