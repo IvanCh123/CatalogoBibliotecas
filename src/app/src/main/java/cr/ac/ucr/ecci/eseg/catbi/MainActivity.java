f2407bbfb2a8746f938f41bf5ee20c4396b9908e
Sebastian Otarola <otarolasebas@hotmail.com>
2020-09-20 17:14:46 -0600

    CNQ - 17 Buscador básico Se revierten cambios de commit anterior porque no se ha podido mostrar el bottom navigation en otras actividades.

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/MainActivity.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/MainActivity.java
@@ -10,8 +10,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ResultadosBusqueda.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ResultadosBusqueda.java
@@ -1,21 +1,10 @@
 package cr.ac.ucr.ecci.eseg.catbi;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.content.Intent;
-import android.view.MenuItem;
 import android.widget.TextView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ResultadosBusqueda extends AppCompatActivity {
 
@@ -25,40 +14,10 @@ public class ResultadosBusqueda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultados_busqueda);
-        BottomNavigationView navView = (BottomNavigationView) findViewById(R.id.nav_view);
         Intent intent = getIntent();
         String message = intent.getStringExtra(MESSAGE_KEY);
         TextView textView = (TextView) findViewById(R.id.txtPalabra);
         textView.setText(message);
-        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemMenu = item.getItemId();
-                Intent intentActPrincipal = new Intent(getBaseContext(), MainActivity.class);
-
-                if(itemMenu == R.id.nav_busqueda){
-                    Fragment mFragment = null;
-                    FragmentManager fragmentManager = getSupportFragmentManager();
-                    //fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
-
-                }else if (itemMenu == R.id.nav_ubicacion){
-
-                }else if (itemMenu == R.id.nav_preguntas){
-
-                }
-                return false;
-            }
-        });
-
-
-
-
-
-
-
-
-
-
 
     }
 
