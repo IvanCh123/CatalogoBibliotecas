82d80e5270b8069479ac10e591825f9a22ff189d
Gerald Bermúdez Ramírez <gerald-1099@hotmail.com>
2020-09-23 21:51:07 -0600

    CNQ-20 Selección de resultados de búsqueda Se implementa el código de la captura de datos y el llenado de los text views. Aún no se ha podido probar bien porque falta la parte del envío de datos y la recuperación de algunos otros.

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/InformacionDetalladaMaterial.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/InformacionDetalladaMaterial.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import cr.ac.ucr.ecci.eseg.catbi.ui.Resultado.Material;
 
 public class InformacionDetalladaMaterial extends AppCompatActivity {
     private static final String TAG = "InformacionDetalladaMaterial";
@@ -15,5 +18,25 @@ public class InformacionDetalladaMaterial extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion_detallada_material);
         Log.d(TAG, "onCreate: called.");
+
+        Material materialRecibido = (Material) getIntent().getSerializableExtra("materialClickeado");
+
+        TextView textViewNombreMaterial = findViewById(R.id.textViewNombreMaterial);
+        TextView textViewContentID = findViewById(R.id.textViewContentID);
+        TextView textViewContentTitulo = findViewById(R.id.textViewContentTitulo);
+        TextView textViewContentAutor = findViewById(R.id.textViewContentAutor);
+        TextView textViewContentColeccion = findViewById(R.id.textViewContentColeccion);
+        TextView textViewContentIdioma = findViewById(R.id.textViewContentIdioma);
+        TextView textViewContentTipo = findViewById(R.id.textViewContentTipo);
+        TextView textViewContentBiblioteca = findViewById(R.id.textViewContentBiblioteca);
+
+        textViewNombreMaterial.setText(materialRecibido.getTitulo());
+        //textViewContentID.setText(materialRecibido.getID());
+        textViewContentTitulo.setText(materialRecibido.getTitulo());
+        textViewContentAutor.setText(materialRecibido.getAutor());
+        textViewContentColeccion.setText(materialRecibido.getColeccion());
+        //textViewContentIdioma.setText(materialRecibido.getIdioma());
+        textViewContentTipo.setText(materialRecibido.getFormato());
+        //textViewContentBiblioteca.setText(materialRecibido.getBiblioteca());
     }
 }
