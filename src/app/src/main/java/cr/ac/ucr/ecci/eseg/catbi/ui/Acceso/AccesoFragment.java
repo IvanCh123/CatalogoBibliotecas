f4703d2078ae7453858b82899d8d69e2d56a41a1
Sebastian Otarola <otarolasebas@hotmail.com>
2020-10-05 15:27:55 -0600

    CNQ - 38 Reservación Material Primer intento de inicio de login en la aplicación

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ui/Acceso/AccesoFragment.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ui/Acceso/AccesoFragment.java
@@ -1,10 +1,14 @@
 package cr.ac.ucr.ecci.eseg.catbi.ui.Acceso;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,24 +16,56 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import cr.ac.ucr.ecci.eseg.catbi.Perfil;
 import cr.ac.ucr.ecci.eseg.catbi.R;
 
 public class AccesoFragment extends Fragment {
 
     private AccesoViewModel accesoViewModel;
+    private FirebaseAuth mAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         accesoViewModel =
                 ViewModelProviders.of(this).get(AccesoViewModel.class);
         View root = inflater.inflate(R.layout.fragment_acceso, container, false);
-        //final TextView textView = root.findViewById(R.id.text_notifications);
-        accesoViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final EditText correoUsuario = root.findViewById(R.id.editTextCorreo);
+        final EditText contrasenaUsuario = root.findViewById(R.id.editTextContrasena);
+        final Button btnInicioSesion = root.findViewById(R.id.btnInicioSesion);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnInicioSesion.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                //textView.setText(s);
+            public void onClick(View view) {
+                String correo = correoUsuario.getText().toString();
+                String password = contrasenaUsuario.getText().toString();
+                autenticarUsuarios(correo,password,view);
+
             }
         });
         return root;
+    }
+
+    public void autenticarUsuarios (String correo, String password, View view){
+        if (correo.isEmpty() || password.isEmpty()){
+            Toast.makeText(view.getContext(),"Por favor digite un correo y una contraseña",Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(correo,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        startActivity(new Intent(getContext(), Perfil.class));
+                    }else {
+                        Toast.makeText(getContext(),"Credenciales inválidas",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
     }
 }
