269f04cd047fecf4bce57c6fb3e3c44a8f57cd30
Sebastian Otarola <otarolasebas@hotmail.com>
2020-10-06 17:40:48 -0600

    CNQ - 41 Reservación Material Se pasan datos al fragmento de perfil una vez el usuario logeado.

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ui/Perfil/PerfilViewModel.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ui/Perfil/PerfilViewModel.java
@@ -1,14 +1,14 @@
-package cr.ac.ucr.ecci.eseg.catbi.ui.Acceso;
+package cr.ac.ucr.ecci.eseg.catbi.ui.Perfil;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class AccesoViewModel extends ViewModel {
+public class PerfilViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public AccesoViewModel() {
+    public PerfilViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("Fragmento de preguntas");
     }
