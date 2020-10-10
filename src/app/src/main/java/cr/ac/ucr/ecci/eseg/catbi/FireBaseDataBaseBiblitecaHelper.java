fb207f0b086e5929239de3e5df5a3545d6dd1bf8
IvanCh123 <ivanjavier2000@gmail.com>
2020-10-10 05:08:15 -0600

    CNQ-55
    Buscador básico
    
    Se realiza el filtrado necesario para cada permutación posible de la búsqueda de material (campo de busqueda y palabra clave). Además se eliminan los carácteres especiales a la hora de filtrar para evitar resultados inesperados.

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/FireBaseDataBaseBiblitecaHelper.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/FireBaseDataBaseBiblitecaHelper.java
@@ -7,6 +7,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +19,7 @@ public class FireBaseDataBaseBiblitecaHelper {
     private DatabaseReference referenciaBiblioteca;
     private DatabaseReference referenciaMaterial;
 
-    private String filtro;
+    private String[] filtro;
     private List<ListarBibliotecas> listaBibliotecas= new ArrayList<>();
     private List<Material> listaMaterial = new ArrayList<>();
 
@@ -41,7 +42,7 @@ public class FireBaseDataBaseBiblitecaHelper {
         database=FirebaseDatabase.getInstance();
         referenciaBiblioteca= database.getReference("Bibliotecas");
         referenciaMaterial= database.getReference("Material");
-        filtro = "";
+        filtro = null;
     }
 
     public void readBibliotecas(final DataStatus dataStatus){
@@ -65,21 +66,20 @@ public class FireBaseDataBaseBiblitecaHelper {
         });
     }
 
-    public void readMaterial(final MaterialDataStatus materialDataStatus, final String filtro){
+    public void readMaterial(final MaterialDataStatus materialDataStatus, final String[] filtro){
         this.filtro = filtro;
         referenciaMaterial.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 listaMaterial.clear();
                 List<String> keys = new ArrayList<>();
+
                 for(DataSnapshot keyNode : dataSnapshot.getChildren()){
                     keys.add(keyNode.getKey());
                     Material material = keyNode.getValue(Material.class);
                     material.setID(keyNode.getKey());
 
-                    if(material.getTitulo().toLowerCase().contains(filtro.toLowerCase())){
-                        listaMaterial.add(material);
-                    }
+                    realizarFiltrado(material, filtro, listaMaterial);
                 }
                 materialDataStatus.DataIsLoaded(listaMaterial,keys);
             }
@@ -87,5 +87,34 @@ public class FireBaseDataBaseBiblitecaHelper {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {}
         });
+    }
+
+    private void realizarFiltrado(Material material, String[] filtro, List<Material> listaMaterial) {
+        String titulo = StringUtils.stripAccents(material.getTitulo()).toLowerCase();
+        String autor = StringUtils.stripAccents(material.getAutor()).toLowerCase();
+        String idioma = StringUtils.stripAccents(material.getIdioma()).toLowerCase();
+
+        switch (filtro[1]){
+            case "titulo":
+                if(titulo.contains(filtro[0])){
+                    listaMaterial.add(material);
+                }
+                break;
+            case "autor":
+                if(autor.contains(filtro[0])){
+                    listaMaterial.add(material);
+                }
+                break;
+            case "idioma":
+                if(idioma.contains(filtro[0])){
+                    listaMaterial.add(material);
+                }
+                break;
+            case "todo":
+                if(titulo.contains(filtro[0]) || autor.contains(filtro[0]) || idioma.contains(filtro[0])){
+                    listaMaterial.add(material);
+                }
+                break;
+        }
     }
 }
