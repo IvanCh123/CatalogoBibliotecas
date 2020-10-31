50ee73c92fbf836d004fe797112d9fb3aca66233
Gerald Bermúdez Ramírez <gerald-1099@hotmail.com>
2020-10-31 14:46:16 -0600

    CNQ - 89. sincronizacion-bases-datos. Se agregan unas líneas que faltaban en la cla clase AppDataBase para que el código base de room referente a Usuario y Reservación esté completo.

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/DataBaseRoom/AppDataBase.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/DataBaseRoom/AppDataBase.java
@@ -3,11 +3,10 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 // Aca tenemos que definir Biblioteca, Material, Usuario....
-@Database(entities = {Biblioteca.class,Material.class}, version = 1)
+@Database(entities = {Biblioteca.class, Material.class, Usuario.class, Reservacion.class}, version = 1)
 public abstract class AppDataBase extends RoomDatabase {
     public abstract BibliotecaDAO bibliotecaDAO();
     public abstract MaterialDAO materialDAO();
-    // public abstract UsuarioDAO  usuarioDAO();
-    // ....
-
+    public abstract UsuarioDAO usuarioDAO();
+    public abstract ReservacionDAO reservacionDAO();
 }
