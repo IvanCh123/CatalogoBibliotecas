e098063c72c5ede31f23d71916c7fb138044777f
Gerald Bermúdez Ramírez <gerald-1099@hotmail.com>
2020-10-31 18:23:38 -0600

    CNQ - 88. sincronizacion-bases-datos. Se agrega el código necesario para insertar los datos de la base de datos de Firebase en la base de datos local.

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/LoginActivity.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/LoginActivity.java
@@ -2,8 +2,10 @@ package cr.ac.ucr.ecci.eseg.catbi;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +20,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.List;
 
+import cr.ac.ucr.ecci.eseg.catbi.DataBaseRoom.AppDataBase;
 import cr.ac.ucr.ecci.eseg.catbi.DataBaseRoom.Biblioteca;
 import cr.ac.ucr.ecci.eseg.catbi.DataBaseRoom.Material;
 import cr.ac.ucr.ecci.eseg.catbi.DataBaseRoom.Reservacion;
 import cr.ac.ucr.ecci.eseg.catbi.DataBaseRoom.Usuario;
 import cr.ac.ucr.ecci.eseg.catbi.FireBaseDataBaseBibliotecaHelper;
+import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -31,46 +35,50 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnInicioSesion;
     private ProgressBar barraProgreso;
     private FireBaseDataBaseBibliotecaHelper mFireBaseDataBaseBibliotecaHelper;
+    private AppDataBase dbLocal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        /*
+        //Se crea la base local
+        dbLocal = Room.databaseBuilder(getApplicationContext(),
+                AppDataBase.class, "bibliotecasStorage").build();
+
         //Se crea instancia del helper
         mFireBaseDataBaseBibliotecaHelper = new FireBaseDataBaseBibliotecaHelper();
         // Obtener datos de Firebase
         //Bibliotecas
-        mFireBaseDataBaseBibliotecaHelper.readBibliotecas(new FireBaseDataBaseBibliotecaHelper.DataStatus(){
+        mFireBaseDataBaseBibliotecaHelper.readAllBibliotecas(new FireBaseDataBaseBibliotecaHelper.AllBibliotecasDataStatus(){
             @Override
-            public void dataLoaded(List<Biblioteca> bibliotecas, List<String> keys) {
+            public void DataIsLoaded(List<Biblioteca> bibliotecas) {
                 insertarBibliotecasBaseLocal(bibliotecas);
             }
         });
         //Usuarios
-        mFireBaseDataBaseBibliotecaHelper.readAllUsuarios(new FireBaseDataBaseBibliotecaHelper.UsuariosDataStatusList(){
+        mFireBaseDataBaseBibliotecaHelper.readAllUsuarios(new FireBaseDataBaseBibliotecaHelper.AllUsuariosDataStatus(){
             @Override
-            public void DataIsLoaded(List<Usuario> usuarios, List<String> keys) {
+            public void DataIsLoaded(List<Usuario> usuarios) {
                 insertarUsuariosBaseLocal(usuarios);
             }
         });
 
         //Reservaciones
-        mFireBaseDataBaseBibliotecaHelper.readAllReservaciones(new FireBaseDataBaseBibliotecaHelper.ReservaDataStatus(){
+        mFireBaseDataBaseBibliotecaHelper.readAllReservaciones(new FireBaseDataBaseBibliotecaHelper.AllReservacionesDataStatus(){
             @Override
-            public void DataIsLoaded(List<Reservacion> reservaciones, List<String> keys) {
+            public void DataIsLoaded(List<Reservacion> reservaciones) {
                 insertarReservacionesBaseLocal(reservaciones);
             }
         });
 
         //Materiales
-        mFireBaseDataBaseBibliotecaHelper.readAllMateriales(new FireBaseDataBaseBibliotecaHelper.MaterialDataStatus(){
+        mFireBaseDataBaseBibliotecaHelper.readAllMateriales(new FireBaseDataBaseBibliotecaHelper.AllMaterialesDataStatus(){
             @Override
-            public void DataIsLoaded(List<Material> materiales, List<String> keys) {
+            public void DataIsLoaded(List<Material> materiales) {
                 insertarMaterialesBaseLocal(materiales);
             }
         });
-        */
+
         mAuth = FirebaseAuth.getInstance();
         correoUsuario = findViewById(R.id.editTextCorreo);
         contrasenaUsuario = findViewById(R.id.editTextContrasena);
@@ -114,18 +122,90 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void insertarBibliotecasBaseLocal(List<Biblioteca> bibliotecas){
-
+        new insertarBibliotecas().execute(bibliotecas);
     }
 
     public void insertarUsuariosBaseLocal(List<Usuario> usuarios){
-
+        new insertarUsuarios().execute(usuarios);
     }
 
     public void insertarReservacionesBaseLocal(List<Reservacion> reservaciones){
-
+        new insertarReservaciones().execute(reservaciones);
     }
 
-    public void insertarMaterialesBaseLocal(List<Material> material){
+    public void insertarMaterialesBaseLocal(List<Material> materiales){
+        new insertarMateriales().execute(materiales);
+    }
 
+    private class insertarBibliotecas extends AsyncTask<List<Biblioteca>, Void, Void> {
+        @Override
+        protected Void doInBackground(List<Biblioteca>... lists) {
+            List <Biblioteca> bibliotecas = lists[0];
+            for(int i = 0; i<bibliotecas.size();i++){
+                Biblioteca biblioteca = (Biblioteca) bibliotecas.get(i);
+                dbLocal.bibliotecaDAO().insertar(biblioteca);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void aVoid) {
+        // Mostrar un mensaje para el usuario
+            Toast.makeText(getApplicationContext(), "Se han insertado las bilbiotecas" , Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class insertarUsuarios extends AsyncTask<List<Usuario>, Void, Void> {
+        @Override
+        protected Void doInBackground(List<Usuario>... lists) {
+            List <Usuario> usuarios = lists[0];
+            for(int i = 0; i<usuarios.size();i++){
+                Usuario usuario = (Usuario) usuarios.get(i);
+                dbLocal.usuarioDAO().insertar(usuario);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void aVoid) {
+            // Mostrar un mensaje para el usuario
+            Toast.makeText(getApplicationContext(), "Se han insertado los usuarios" , Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class insertarReservaciones extends AsyncTask<List<Reservacion>, Void, Void> {
+        @Override
+        protected Void doInBackground(List<Reservacion>... lists) {
+            List <Reservacion> reservaciones = lists[0];
+            for(int i = 0; i<reservaciones.size();i++){
+                Reservacion reservacion = (Reservacion) reservaciones.get(i);
+                dbLocal.reservacionDAO().insertar(reservacion);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void aVoid) {
+            // Mostrar un mensaje para el usuario
+            Toast.makeText(getApplicationContext(), "Se han insertado las reservaciones" , Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class insertarMateriales extends AsyncTask<List<Material>, Void, Void> {
+        @Override
+        protected Void doInBackground(List<Material>... lists) {
+            List <Material> materiales = lists[0];
+            for(int i = 0; i<materiales.size();i++){
+                Material material = (Material) materiales.get(i);
+                dbLocal.materialDAO().insertar(material);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void aVoid) {
+            // Mostrar un mensaje para el usuario
+            Toast.makeText(getApplicationContext(), "Se han insertado los materiales" , Toast.LENGTH_LONG).show();
+        }
     }
 }
