c1ad3bf4f7c640a8b37379c9f64bbadd9ca07e93
Gerald Bermúdez Ramírez <gerald-1099@hotmail.com>
2020-11-22 00:38:55 -0600

    CNQ - 139. Reservación material. Se integra el menú del cerrar sesión al fragmento de perfil.

--- src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ui/Perfil/PerfilFragment.java
+++ src/app/src/main/java/cr/ac/ucr/ecci/eseg/catbi/ui/Perfil/PerfilFragment.java
@@ -3,7 +3,11 @@ package cr.ac.ucr.ecci.eseg.catbi.ui.Perfil;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -27,9 +31,11 @@ import cr.ac.ucr.ecci.eseg.catbi.BaseDatos.FireBaseDataBaseBiblitecaHelper;
 import cr.ac.ucr.ecci.eseg.catbi.R;
 import cr.ac.ucr.ecci.eseg.catbi.DataBaseRoom.Usuario;
 import cr.ac.ucr.ecci.eseg.catbi.DataBaseRoom.Reservacion;
+import cr.ac.ucr.ecci.eseg.catbi.ui.Busqueda.BusquedaFragment;
 
 
 import android.content.Context;
+import android.widget.Toast;
 
 public class PerfilFragment extends Fragment {
 
@@ -56,7 +62,7 @@ public class PerfilFragment extends Fragment {
         perfilViewModel =
                 ViewModelProviders.of(this).get(PerfilViewModel.class);
         final View root = inflater.inflate(R.layout.fragment_perfil, container, false);
-
+        setHasOptionsMenu(true);
         nombreUsuario =  (TextView) root.findViewById(R.id.txtNombre);
         correoUsuario = (TextView) root.findViewById(R.id.txtCorreo);
         mRecyclerView = (RecyclerView) root.findViewById(R.id.reservacionesRecyclerView);
@@ -126,6 +132,24 @@ public class PerfilFragment extends Fragment {
         boolean isConnected = activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
         return isConnected;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_perfil, menu);
+        PerfilFragment.super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.cerrar_sesion) {
+            Toast.makeText(getContext(), "Cerrar Sesion",Toast.LENGTH_LONG).show();
+            return true;
+        }else{
+            Log.v("g","y");
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
