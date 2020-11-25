01ca62ccecbed53d49185f304da87c423f59b3f8
jvs1991 <josue.valverde1@gmail.com>
2020-11-25 17:23:53 -0600

    CNQ-115 Eliminar reservaciones
    Finalizacion de prueba de eliminacion de reserva

--- src/app/src/androidTest/java/cr/ac/ucr/ecci/eseg/catbi/EliminarReservaTest.java
+++ src/app/src/androidTest/java/cr/ac/ucr/ecci/eseg/catbi/EliminarReservaTest.java
@@ -1,11 +1,15 @@
 package cr.ac.ucr.ecci.eseg.catbi;
 
+import android.widget.ListView;
+
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 
+import cr.ac.ucr.ecci.eseg.catbi.ui.Administrar.BorrarReservas;
 import cr.ac.ucr.ecci.eseg.catbi.ui.Administrar.ReservaFila;
 
 import static androidx.test.espresso.Espresso.onData;
@@ -13,12 +17,14 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.Matchers.allOf;
 
 public class EliminarReservaTest {
     public static final String TEST_STRING_USUARIO="Estudiante";
@@ -38,18 +44,23 @@ public class EliminarReservaTest {
         Thread.sleep(5000);
         onView(withText(TEST_STRING_MAIL)).perform(click());
         Thread.sleep(5000);
+        onView(allOf(withId(R.id.libro_reserva), isDisplayed()));
+        Thread.sleep(5000);
         onView(withId(R.id.select_all)).perform(click());
-        //onView(withText("Cocori"));
-        //onView(withId(R.id.recyclerViewEliminarReserva)).check(matches(withText("Cocori")));
-        //onData().inAdapterView(withId(R.id.recyclerViewEliminarReserva)).check(matches(isDisplayed()));
-               /* instanceOf(ReservaFila.class))
-                .atPosition(i)
-                .perform(click());*/
         Thread.sleep(10000);
         onView(withId(R.id.aceptar_eliminar)).perform(click());
         onView(withId(android.R.id.button1)).perform(click());
+        Thread.sleep(30000);
+
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Eliminar reserva")).perform(click());
         Thread.sleep(5000);
-        //onView(withId(R.id.editTextTituloFrase)).perform(replaceText(TEST_STRING_TITULO));
+        onView(withId(R.id.nombre_usuario)).perform(replaceText(TEST_STRING_USUARIO));
+        onView(withId(R.id.usuario_buscar)).perform(click());
+        Thread.sleep(5000);
+        onView(withText(TEST_STRING_MAIL)).perform(click());
+        Thread.sleep(5000);
+        onView(allOf(withId(R.id.libro_reserva), isDisplayed()));
     }
 
 }
